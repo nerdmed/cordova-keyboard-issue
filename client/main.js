0c86b3c481e0d94ae08f90cb1401683bf188c6d0
Ahmed Hassan <a.hassan@buismed.de>
2016-09-18 16:27:23 +0200

    inital commit

---
+++ client/main.js
@@ -0,0 +1,27 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+  showInput(){
+  	return Template.instance().counter.get() % 2 === 0;
+  }
+});
+
+Template.hello.events({
+  'click button, submit #my-form'(event, instance) {
+  	event.preventDefault();
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
